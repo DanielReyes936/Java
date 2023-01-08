8e32d2eb9923d9e2b1444bc9264b52504851fa29
Daniel Reyes Nancupil <reyesnan@sheridancollege.ca>
2023-01-08 12:42:15 -0500

    Add files via upload

---
+++ ArrayList/test/arraylist/AIPlayerTest.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package arraylist;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author tmdan
+ */
+public class AIPlayerTest {
+    
+    public AIPlayerTest() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+
+    /**
+     * Test of haveTurn method, of class AIPlayer.
+     */
+    @Test
+    public void testHaveTurn() {
+        System.out.println("haveTurn");
+        AIPlayer instance = new AIPlayer();
+        instance.haveTurn();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
---
+++ ArrayList/test/arraylist/GoFishTest.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package arraylist;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author tmdan
+ */
+public class GoFishTest {
+    
+    public GoFishTest() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+
+    /**
+     * Test of draw method, of class GoFish.
+     */
+    
+
+    /**
+     * Test of deckSize method, of class GoFish.
+     */
+    @Test
+    public void testDeckSize() {
+        System.out.println("deckSize");
+        int expResult = 0;
+        int result = GoFish.deckSize();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of main method, of class GoFish.
+     */
+   
+    
+}
