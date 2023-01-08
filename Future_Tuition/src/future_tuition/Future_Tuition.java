c054b48678d21551ad8f094afb34a2cdcfdb78e7
Daniel Reyes Nancupil <reyesnan@sheridancollege.ca>
2023-01-08 12:52:28 -0500

    Add files via upload

---
+++ Future_Tuition/src/future_tuition/Future_Tuition.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package future_tuition;
+
+/**
+ *
+ * @author tmdan
+ */
+public class Future_Tuition {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+       double tuition =10000;
+       int year =0;
+       
+        
+       for (tuition = tuition +1.07;year ++)
+           System.out.print(" year 1 is : ");
+       
+       
+       //tuition = tuition +1.07;year ++;
+      // tuition = tuition +1.07;year ++;
+        // TODO code application logic here
+    }
+    
+}
